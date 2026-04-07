c13af74d2913df403e07deb0cef9ccb183fe4378
Farid Carrasco <karrazgommfarid@gmail.com>
2026-04-07 12:29:26 -0600

    feat: Comando Help

--- commands/slash/help.js
+++ commands/slash/help.js
@@ -12,33 +12,26 @@ module.exports = {
         let db = await tools.fetchSettings(int.user.id, int.guild.id);
         let serverLang = db?.settings?.lang || config.defaultLanguage || 'en';
 
-        // 1. Obtener todos los Slash Commands registrados
         let commands = Array.from(client.application.commands.cache.values())
-            .filter(cmd => cmd.type === 1); // 1 = ChatInput (Comandos de barra)
+            .filter(cmd => cmd.type === 1); 
 
-        // 2. Ordenarlos alfabéticamente
         commands.sort((a, b) => a.name.localeCompare(b.name));
 
-        // 3. Formatear la lista usando las traducciones en vivo
         let commandList = commands.map(cmd => {
-            // Etiqueta nativa clickeable de Discord
             let tag = `</${cmd.name}:${cmd.id}>`;
             
-            // Buscar la traducción dinámica. Si no existe, usar la descripción cruda de Discord.
             let desc = t(`commands.${cmd.name}.metadata_description`, {}, serverLang);
             if (desc === `commands.${cmd.name}.metadata_description`) desc = cmd.description;
             
             return `${tag} - ${desc}`;
         });
 
-        // 4. Crear el Embed base
         let embed = tools.createEmbed({
             title: t('commands.help.title', {}, serverLang),
             color: tools.COLOR,
             author: { name: client.user.username, iconURL: client.user.displayAvatarURL() }
         });
 
-        // 5. Instanciar el paginador interactivo
         let pager = new PageEmbed(embed, commandList, {
             size: 8, 
             owner: int.user.id, 
@@ -47,7 +40,6 @@ module.exports = {
             lang: serverLang
         });
 
-        // 6. Enviar al chat
         pager.post(int);
     }
 }
